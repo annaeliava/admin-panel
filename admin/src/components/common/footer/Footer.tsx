919f03829a71e888c79f78a139118ed7598e34c1
annaeliava <annaeliavad@gmail.com>
2023-09-24 02:25:14 +0300

    changed footer's styles

--- admin/src/components/common/footer/Footer.tsx
+++ admin/src/components/common/footer/Footer.tsx
@@ -1,9 +1,7 @@
-import React from "react"
-
 const Footer = () => {
     return (
         <>
-            <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+            <footer className="bg-white">
                 <div className="flex-row justify-center w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center">
                     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                         © 2023 Dashboard™
