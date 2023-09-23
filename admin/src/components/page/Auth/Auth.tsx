2222c9ee689139d28cfbcc309c0d5960a341fcd8
annaeliava <annaeliavad@gmail.com>
2023-09-24 02:27:20 +0300

    added react-hook-form

--- admin/src/components/page/Auth/Auth.tsx
+++ admin/src/components/page/Auth/Auth.tsx
@@ -1,6 +1,48 @@
-import React from "react"
+import {
+    useForm,
+    SubmitHandler
+} from "react-hook-form"
+import { useNavigate } from "react-router-dom"
+
+type FormInputs = {
+    email: string
+    password: string
+}
 
 function Auth() {
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm<FormInputs>({
+        defaultValues: {
+            email: '',
+            password: '',
+        }
+    })
+
+    const navigate = useNavigate()
+
+    const onSubmit: SubmitHandler<FormInputs> = (data) => {
+        if (data.email === 'email@example.com' && data.password === '1111') {
+            navigate('/admin/dashboard')
+        } else if (data.email !== 'email@example.com' && data.password !== '1111') {
+            setError('email', {
+                type: 'manual',
+            })
+            setError('password', {
+                type: 'manual',
+            })
+        } else if (data.password !== '1111') {
+            setError('password', {
+                type: 'manual',
+            })
+        } else setError('email', {
+            type: 'manual',
+        })
+    }
+
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -11,41 +53,52 @@ function Auth() {
                         alt="Your Company"
                     />
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Sign in to your account
+                        Войти в аккаунт
                     </h2>
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form className="space-y-6" action="#" method="POST">
+                    <form
+                        onSubmit={handleSubmit(onSubmit)}
+                        className="space-y-6"
+                        action="#"
+                        method="POST">
                         <div>
                             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                Email address
+                                Логин
                             </label>
                             <div className="mt-2">
                                 <input
+                                    {...register("email", {
+                                        required: true,
+                                        pattern: {
+                                            value: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                            message: "Введите верный логин",
+                                        },
+                                    })}
                                     id="email"
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                    required
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
+                                {errors?.email && (
+                                    <p className="text-red-600" role="alert">Введите верный логин</p>
+                                )}
+
                             </div>
                         </div>
 
                         <div>
                             <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                Password
+                                Пароль
                             </label>
                             <div className="mt-2">
                                 <input
+                                    {...register("password", { required: true })}
                                     id="password"
-                                    name="password"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    required
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
+                                {errors?.password && (
+                                    <p className="text-red-600" role="alert">Введите верный пароль</p>
+                                )}
                             </div>
                         </div>
 
@@ -54,7 +107,7 @@ function Auth() {
                                 type="submit"
                                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
-                                Sign in
+                                Войти
                             </button>
                         </div>
                     </form>
