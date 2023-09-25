e679cbebc7e467d1d6ca9f5c49c4f89ab6bb4e28
annaeliava <annaeliavad@gmail.com>
2023-09-25 21:08:54 +0300

    fixed headings

--- admin/src/components/page/Orders/Headings/Headings.tsx
+++ admin/src/components/page/Orders/Headings/Headings.tsx
@@ -9,10 +9,6 @@ const Headings = () => {
                     </th>
                     <th
                         className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Дата заказа
-                    </th>
-                    <th
-                        className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                         Заказчик
                     </th>
                     <th
