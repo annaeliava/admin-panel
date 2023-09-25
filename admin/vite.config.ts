3c01e06c42686eec1ae26899c87827d649603074
annaeliava <annaeliavad@gmail.com>
2023-09-25 21:30:06 +0300

    changed base

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://annaeliava.github.io/admin-panel/",
+  base: "/admin-panel",
 })
